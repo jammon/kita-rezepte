c284415f2e2fb0bd0dc4b4335d6c1102fb1a1719
Johannes Ammon <j.ammon@dr-ammon.de>
2019-06-01 14:16:13 +0200

    DebugToolbar only in Development

--- kitarezepte/kitarezepte/settings.py
+++ kitarezepte/kitarezepte/settings.py
@@ -92,7 +92,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'taggit',
     'rezepte.apps.RezepteConfig',
-    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -103,8 +102,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
+if DEBUG:
+    INSTALLED_APPS.append('debug_toolbar')
+    MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
 ROOT_URLCONF = 'kitarezepte.urls'
 
