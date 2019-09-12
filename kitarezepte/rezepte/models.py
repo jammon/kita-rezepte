6016d15c84ae3dc531d4b3935566b372a3851654
Johannes Ammon <j.ammon@dr-ammon.de>
2019-09-12 19:11:45 +0200

    Manches kann auch null sein

--- kitarezepte/rezepte/models.py
+++ kitarezepte/rezepte/models.py
@@ -49,7 +49,7 @@ class Client(models.Model):
         max_length=100, 
         help_text='z.B. "Reis Teigwaren Getreide Kartoffeln Gemüse '
             'Suppe Fischgericht Lieblingsgericht"',
-        default="")
+        default="", blank=True, null=True)
 
     class Meta:
         verbose_name = "Mandant"
@@ -168,8 +168,6 @@ class Rezept(models.Model):
         max_length=100, blank=True, help_text='wird i.d.R. aus titel berechnet')
     fuer_kinder = models.IntegerField(help_text="Anzahl der Kinder")
     fuer_erwachsene = models.IntegerField(help_text="Anzahl der Erwachsenen")
-    # zutaten = models.ManyToManyField(
-    #     Zutat, through='RezeptZutat', related_name='rezepte')
     zubereitung = models.TextField()
     anmerkungen = models.TextField(null=True, blank=True)
     eingegeben_von = models.ForeignKey(User, on_delete=models.SET_NULL,
@@ -180,7 +178,7 @@ class Rezept(models.Model):
         max_length=60,
         help_text='Die Kategorie, zu der das Rezept gehört, '
         'ggf. eine Leerzeichen-getrennte Liste mehrerer Kategorien',
-        default='')
+        default='', blank=True, null=True)
     # z.B. Gemüse, Teigwaren, Suppe, Getreide, Reis usw.
     _preis = models.IntegerField(
         default=KEIN_PREIS,
