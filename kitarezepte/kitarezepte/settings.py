31c2b96b4a1a9478fc2c2d65b84e95d983952005
Johannes Ammon <j.ammon@dr-ammon.de>
2020-06-13 05:32:54 +0200

    Fehler bei Zutaten-Darstellung

--- kitarezepte/kitarezepte/settings.py
+++ kitarezepte/kitarezepte/settings.py
@@ -111,6 +111,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'tinymce',
     'taggit',
     'rezepte.apps.RezepteConfig',
