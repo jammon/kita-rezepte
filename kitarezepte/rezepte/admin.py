2199719b457dfd9d2a4f91333f365decd2913ef4
Johannes Ammon <j.ammon@dr-ammon.de>
2020-06-21 19:56:26 +0200

    Zutaten vorbelegen

--- kitarezepte/rezepte/admin.py
+++ kitarezepte/rezepte/admin.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin
+import csv, os
+
+from django.conf import settings
+from django.contrib import admin, messages
 
 from .models import Zutat, Rezept, RezeptZutat, GangPlan, Client, Editor
 
 
 class ZutatAdmin(admin.ModelAdmin):
     list_display = ('name', 'kategorie')
-    list_filter = ('kategorie', )
+    list_filter = ('kategorie', 'client__name')
 
 
 class RezeptZutatInline(admin.TabularInline):
@@ -56,14 +59,48 @@ class RezeptAdmin(admin.ModelAdmin):
 
 class GangPlanAdmin(admin.ModelAdmin):
     ordering = ('datum',)
+    list_filter = ('gang', 'client__name')
+    date_hierarchy = 'datum'
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related('rezept')
+
+
+class ClientAdmin(admin.ModelAdmin):
+    actions = ['import_zutaten']
+
+    def import_zutaten(self, request, queryset):
+        with open(os.path.join(settings.BASE_DIR, 'zutaten.csv'),
+                  'r') as csvfile:
+            zutatenreader = csv.reader(csvfile, delimiter=';')
+            zutaten_list = list(zutatenreader)
+
+        zutaten = []
+        for client in queryset:
+            if Zutat.objects.filter(client=client).count() > 0:
+                self.message_user(
+                    request,
+                    f"Client '{client.name}' hat schon Zutaten eingegeben",
+                    messages.ERROR)
+                return
+            for z in zutaten_list:
+                if len(z) == 5:
+                    zutaten.append(Zutat(
+                        name=z[0],
+                        client=client,
+                        einheit=z[1],
+                        menge_pro_einheit=z[2],
+                        masseinheit=z[3],
+                        kategorie=z[4]))
+        Zutat.objects.bulk_create(zutaten)
+        self.message_user(
+            request, "Zutaten wurden importiert", messages.SUCCESS)
+    import_zutaten.short_description = "Standardzutaten für neuen Client importieren"
 
 
 admin.site.register(Zutat, ZutatAdmin)
 admin.site.register(Rezept, RezeptAdmin)
 admin.site.register(RezeptZutat)
 admin.site.register(GangPlan, GangPlanAdmin)
-admin.site.register(Client)
+admin.site.register(Client, ClientAdmin)
 admin.site.register(Editor)
